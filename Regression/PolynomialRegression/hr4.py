49424acd3947b0626c36a56fab2e44f4c9f1cffd
Syed Hashim Reja <hashimreja2@gmail.com>
2018-12-16 00:46:43 +0530

    Add files via upload

---
+++ Regression/PolynomialRegression/hr4.py
@@ -0,0 +1,49 @@
+# Data Preprocessing Template
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, -1].values
+
+# Splitting the dataset into the Training set and Test set
+'''from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)'''
+
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)"""
+
+from sklearn.linear_model import LinearRegression
+linreg = LinearRegression()
+linreg.fit(X,y)
+y_pred = linreg.predict(X)
+
+plt.scatter(X,y,color = 'blue')
+plt.plot(X,linreg.predict(X),color = 'red')
+plt.show()
+
+from sklearn.preprocessing import PolynomialFeatures
+polyreg = PolynomialFeatures(degree = 4)
+X_poly = polyreg.fit_transform(X)
+
+linreg1 = LinearRegression()
+linreg1.fit(X_poly,y)
+
+plt.scatter(X,y,color='blue')
+plt.plot(X,linreg1.predict(polyreg.fit_transform(X)),color='red')
+plt.show()
+#for a smoother curve
+X_grid = np.arange(min(X),max(X),0.1)
+X_grid = X_grid.reshape((len(X_grid),1))
+plt.scatter(X,y,color='red')
+plt.plot(X_grid,linreg1.predict(polyreg.fit_transform(X_grid)),color='red')
+plt.show()
